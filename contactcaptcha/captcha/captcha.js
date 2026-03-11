21c7ad302dc4a858ed9df6bf73239f37c290d4a5
Ps18bhardwaj <sachin.27366@ggnindia.dronacharya.info>
2026-03-11 09:28:02 +0530

    Delete contactcaptcha/captcha directory

--- contactcaptcha/captcha/captcha.js
+++
@@ -1,7 +0,0 @@
-/*################ Developed By: Sanjoy Pal ######################*/
-function refreshCaptcha(sufVal) {
-    sufVal = (sufVal == undefined) ? '' : sufVal;
-    var img = document.images['captcha' + sufVal];
-    if (img != undefined)
-        img.src = img.src.substring(0, img.src.lastIndexOf("?")) + "?rand=" + Math.random() * 1000 + "&sval=" + sufVal;
-}
